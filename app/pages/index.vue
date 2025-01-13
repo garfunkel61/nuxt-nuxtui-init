08eeaa41a422678815bab5b8c1ee47f746afe2d1
Andrzej Dlubak <andrzejdlubak@mbp-andrzej.home>
2025-01-13 15:56:54 +0100

    Set up tailwind and primeUi elems.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1 class="text-primary font-bold underline">Hello World</h1>
+    <Button label="Verify" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
+import Aura from "@primevue/themes/aura";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      ripple: true,
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
   compatibilityDate: "2024-11-01",
-
   future: {
     compatibilityVersion: 4,
   },
-
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
 });
