875f71aa34c191af5b90eac6f3c5b64c9a0a077d
Andrzej Dlubak <andrzejdlubak@mbp-andrzej.home>
2025-01-15 17:51:22 +0100

    Removed prime UI, installed nuxt UI.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,5 @@
-import Aura from "@primevue/themes/aura";
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module", "@nuxt/content"],
-  primevue: {
-    options: {
-      ripple: true,
-      theme: {
-        preset: Aura,
-      },
-    },
-  },
-  css: ["primeicons/primeicons.css"],
+  modules: ["@nuxt/content", "@nuxt/ui"],
   compatibilityDate: "2024-11-01",
   future: {
     compatibilityVersion: 4,
